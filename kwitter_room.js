364fa917b68c1b6d5d121903fb9ec821555fd54a
Karthik-24-Coding <73743387+Karthik-24-Coding@users.noreply.github.com>
2021-10-21 19:00:33 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDfAnobM2enlAIP5I_TD2JTpAcWdCiT-yw",
+    authDomain: "kwitter-7171c.firebaseapp.com",
+    projectId: "kwitter-7171c",
+    storageBucket: "kwitter-7171c.appspot.com",
+    messagingSenderId: "39789923879",
+    appId: "1:39789923879:web:06a0d41fd9c9d445524802"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
